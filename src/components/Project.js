5caa8609855a76517d02c0082e3ad4b2c7125fb3
Stephen Tran <transtephen@Stephens-MacBook-Pro.local>
2023-04-13 23:11:56 -0400

    Fix flip animation glitch

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,29 +7,32 @@ export default function Project() {
         return (
             <div className='project-box mb-10'>
 
-                <div className='project-item'>
 
-                    <div className='project-front'>
-                        <img src={require(`../media/${projectItem.image}`)} alt='' className=' project-image'></img>
-                        <div className='not-image'>
-                            <h2 className='project-name text-center my-2 text-xl'>{projectItem.name}</h2>
-                            <p className='project-description text-slate-700'>{projectItem.description}</p>
+                    
+                    <div className='project-item'>
+
+                        <div className='project-front'>
+                            <img src={require(`../media/${projectItem.image}`)} alt='' className=' project-image'></img>
+                            <div className='not-image'>
+                                <h2 className='project-name text-center my-2 text-xl'>{projectItem.name}</h2>
+                                <p className='project-description text-slate-700'>{projectItem.description}</p>
+                            </div>
+                        </div>
+
+
+
+
+
+                        <div className='project-back'>
+                            <img src={require(`../media/${projectItem.image}`)} alt='' className=' project-image'></img>
+                            <div className='not-image'>
+                                <h2 className='project-name text-center my-2 text-xl'>aaa</h2>
+                                <p className='project-description text-slate-700'>hahahihi</p>
+                            </div>
                         </div>
                     </div>
 
-                    <div className='project-back'>
 
-                        <img src={require(`../media/${projectItem.image}`)} alt='' className=' project-image'></img>
-                        <div className='not-image'>
-                            <h2 className='project-name text-center my-2 text-xl'>aaa</h2>
-                            <p className='project-description text-slate-700'>hahahihi</p>
-                        </div>
-
-                    </div>
-
-
-
-                </div>
 
 
             </div>
