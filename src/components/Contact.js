b5e8b32851f9079f30dbd7a91ec5de342917f14a
Stephen Tran <transtephen@Stephens-MacBook-Pro.local>
2023-04-16 15:34:14 -0400

    minor change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ export default function Contact() {
             <div className='container contacts mx-auto'>
                 {/* ON LEFT */}
 
-                <form className="mx-auto block px-3 md:px-10 md:w-8/12 client-contact-form" action="https://formsubmit.co/sttr2130@gmail.com" method="POST">
+                <form className="mx-auto block px-3 md:px-10 md:w-8/12 client-contact-form" target="_blank" action="https://formsubmit.co/sttr2130@gmail.com" method="POST">
 
                     <input className='form-item my-5 form-client-name ' type="text" name="name" placeholder='Name' required />
 
