0f6f036e13bb868cd4598d6aeac3c5f1a6cebf09
Stephen Tran <transtephen@Stephens-MacBook-Pro.local>
2023-04-16 15:20:13 -0400

    minor change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,20 +12,19 @@ export default function Contact() {
             <div className='container contacts mx-auto'>
                 {/* ON LEFT */}
 
-                <form className="mx-auto block px-3 md:px-10 md:w-8/12 client-contact-form" action="https://formsubmit.co/transtephen2402@gmail.com" method="POST">
-                    
-                    <div className='form-client-name my-5'>
-                        <input className='form-item block' type="text" name="first-name" placeholder='First Name' required />
-                        <input className='form-item block' type="text" name="last-name" placeholder='Last Name' required />
+                <form className="mx-auto block px-3 md:px-10 md:w-8/12 client-contact-form" action="https://formsubmit.co/sttr2130@gmail.com" method="POST">
+
+                    <input className='form-item my-5 form-client-name ' type="text" name="name" placeholder='Name' required />
+
+                    <input className='form-item my-5 form-client-subject' type="text" name="subject" placeholder='Subject' required />
+
+                    <div className='my-5 form-client-information'>
+                        <input className='form-item' type="email" name="email" placeholder="Email" required />
+                        <input className='form-item' type="phone-number" name="phone-number" placeholder="Phone Number" required />
                     </div>
 
-                    <div className='form-client-information my-5'>
-                        <input className='form-item block' type="email" name="email" placeholder="Email" required />
-                        <input className='form-item block' type="subject" name="subject" placeholder="Phone Number" required />
-                    </div>
-
-                    <textarea className='form-item my-5 block form-client-content' type="content" name="content" placeholder="Message" required />
-                    <button className='form-item block form-client-submit mx-auto' type="submit">Send</button>
+                    <textarea className='form-item my-5 form-client-content' type="content" name="content" placeholder="Message" required />
+                    <button className='form-item mx-auto form-client-submit ' type="submit">Send</button>
                 </form>
 
 
