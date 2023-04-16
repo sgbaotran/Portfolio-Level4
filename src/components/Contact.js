ef94ad0f06e7ed448f53eebcac3c75c480281c00
Stephen Tran <transtephen@Stephens-MacBook-Pro.local>
2023-04-16 15:37:02 -0400

    minor change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,8 @@ export default function Contact() {
             <div className='container contacts mx-auto'>
                 {/* ON LEFT */}
 
-                <form className="mx-auto block px-3 md:px-10 md:w-8/12 client-contact-form" target="_blank" action="https://formsubmit.co/sttr2130@gmail.com" method="POST">
+                <form className="mx-auto block px-3 md:px-10 md:w-8/12 client-contact-form" name="_captcha" value="false" action="https://formsubmit.co/sttr2130@gmail.com" method="POST">
+                    <input type="hidden" name="_next" value="https://gbaotran.netlify.app/"/>
 
                     <input className='form-item my-5 form-client-name ' type="text" name="name" placeholder='Name' required />
 
