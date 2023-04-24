ebec4c1d652c5f1afca8d18b634d349174fa053b
Stephen Tran <transtephen@Stephens-MBP.ht.home>
2023-04-24 19:07:52 -0400

    Prevent OVERscroll

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,7 +12,7 @@ export default function Experience(){
             
             <div className = {className}>
                 {/* ??? */}
-                <img src={require(`../media/${timeLineItem.image}`)} alt='' className=' timeline-icon' /> 
+                <img src={require(timeLineItem.image)} alt='' className=' timeline-icon' /> 
 
                 
                 <div className='timeline-item-container shadow-2xl'>
